175b7a275c188fa1150539e90eee30b6b6e8bf14
Théo Ischia <theoischia@MacBook-Air-de-Theo.local>
2024-11-25 17:30:06 +0100

    Verbosity is now global

---
+++ global_value.h
@@ -0,0 +1,11 @@
+// Define for verbosity 
+
+// Low verbosity : very concise
+#define LOW 1 
+// Medium verbosity : synthetic
+#define MEDIUM 2
+// high verbosity : complete
+#define HIGH 3
+
+// Give the level of verbosity needed
+int verbosity;
