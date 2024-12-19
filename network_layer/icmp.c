19b0262ce7b45cffeeda3acbc34d0f758a8cf9a9
Théo Ischia <theoischia@gmail.com>
2024-12-19 22:35:03 +0100

    ICMP verbose

--- network_layer/icmp.c
+++ network_layer/icmp.c
@@ -5,48 +5,46 @@ void handle_icmp_type(const struct icmp *icmp_header) {
     printf("Message: ");
     switch (icmp_header->icmp_type) {
         case ICMP_ECHOREPLY:
-            printf("Echo Reply ");
-            printf("ID: %u, Sequence: %u", 
+            printf("Echo Reply, ");
+            printf("id: %u, Sequence: %u, ", 
                 ntohs(icmp_header->icmp_id), ntohs(icmp_header->icmp_seq));
             break;
         case ICMP_ECHO:
-            printf("Echo Request ");
-            printf("ID: %u, Sequence: %u ", 
+            printf("Echo Request, ");
+            printf("id: %u, Sequence: %u, ", 
                 ntohs(icmp_header->icmp_id), ntohs(icmp_header->icmp_seq));
             break;
         case ICMP_UNREACH:
-            printf("Destination Unreachable ");
-            printf("Code Explanation: ");
+            printf("Destination Unreachable: ");
             switch (icmp_header->icmp_code) {
                 case ICMP_UNREACH_NET:
-                    printf("Network Unreachable ");
+                    printf("Network Unreachable, ");
                     break;
                 case ICMP_UNREACH_HOST:
-                    printf("Host Unreachable ");
+                    printf("Host Unreachable, ");
                     break;
                 case ICMP_UNREACH_PROTOCOL:
-                    printf("Protocol Unreachable ");
+                    printf("Protocol Unreachable, ");
                     break;
                 case ICMP_UNREACH_PORT:
-                    printf("Port Unreachable ");
+                    printf("Port Unreachable, ");
                     break;
                 default:
-                    printf("Unknown code %u ", icmp_header->icmp_code);
+                    printf("Unknown code, %u ", icmp_header->icmp_code);
                     break;
             }
             break;
         case ICMP_TIMXCEED:
-            printf("Time Exceeded  ");
-            printf("Code Explanation: ");
+            printf("Time Exceeded:  ");
             if (icmp_header->icmp_code == ICMP_TIMXCEED_INTRANS)
-                printf("TTL Expired in Transit ");
+                printf("TTL Expired in Transit, ");
             else if (icmp_header->icmp_code == ICMP_TIMXCEED_REASS)
-                printf("Fragment Reassembly Time Exceeded ");
+                printf("Fragment Reassembly Time Exceeded, ");
             else
-                printf("Unknown code %u ", icmp_header->icmp_code);
+                printf("Unknown code %u, ", icmp_header->icmp_code);
             break;
         default:
-            printf("Other ICMP Type ");
+            printf("Other ICMP Type, ");
             break;
     }
 }
@@ -58,10 +56,9 @@ void parse_icmp(const u_char *packet, size_t header_size) {
 
     // Display ICMP header information
     printf("ICMP: ");
-    printf("Type: %u ", icmp_header->icmp_type);
-    printf("Code: %u ", icmp_header->icmp_code);
-    printf("Checksum: 0x%04x ", ntohs(icmp_header->icmp_cksum));
+    handle_icmp_type(icmp_header);
+    printf("Length: %zu, ", packet_size - header_size);
+    if(verbosity >= MEDIUM) printf("Checksum: 0x%04x ", ntohs(icmp_header->icmp_cksum));
 
     // Call the function to handle ICMP type
-    handle_icmp_type(icmp_header);
 }
